6f032bc09b8c7797e985e72b95ccfeb31176a147
TsioryR98 <hei.tsiory.2@gmail.com>
2024-09-16 22:21:34 +0200

    feat:add database JDBC conf only for test and methods for student Repository

--- src/main/java/Repository/DataBaseConnect.java
+++ src/main/java/Repository/DataBaseConnect.java
@@ -1,5 +1,23 @@
 package Repository;
 
-public class DataBaseConnect {
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Component
+public class DataBaseConnect {
+    @Bean
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/todolist", "postgres", "tsiory98");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
 }
+
--- src/main/java/Repository/StudentRepository.java
+++ src/main/java/Repository/StudentRepository.java
@@ -1,18 +1,62 @@
 package Repository;
 
+import Models.AcademicYear;
 import Models.Student;
 
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class StudentRepository implements StudentDAO{
+    DataBaseConnect dataBaseConnect = new DataBaseConnect();
+
     @Override
     public void create(Student newStudent) {
+        String query = "INSERT INTO student (id_student, last_name, first_name, date_of_birth, email, phone_number, enrollment_date, academic_year ) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        try (Connection conn = dataBaseConnect.getConnection();
+             PreparedStatement statement = conn.prepareStatement(query)) {
 
+            statement.setInt(1, newStudent.getStudentId());
+            statement.setString(2, newStudent.getLastName());
+            statement.setString(3, newStudent.getFirstName());
+            statement.setObject(4, newStudent.getDateOfBirth());
+            statement.setString(5, newStudent.getStudentEmail());
+            statement.setString(6, newStudent.getPhoneNumber());
+            statement.setObject(7, newStudent.getEnrollmentDate());
+            statement.setString(8, newStudent.getAcademicYear().name());
+
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException("Error creating newStudent", e);
+        }
     }
 
     @Override
     public List<Student> showAll() {
-        return List.of();
+        List<Student> studentList = new ArrayList<>();
+
+        try (Statement stm = dataBaseConnect.getConnection().createStatement()) {
+            String query = "SELECT * FROM student";
+            ResultSet result = stm.executeQuery(query);
+
+            while (result.next()) {
+                Student studentAdd = new Student(
+                        result.getInt("id"),
+                        result.getString("last_name"),
+                        result.getString("first_name"),
+                        result.getObject("date_of_birth", LocalDateTime.class),
+                        result.getString("email"),
+                        result.getString("phone_number"),
+                        result.getObject("enrollment_date", LocalDateTime.class),
+                        AcademicYear.valueOf(result.getString("academic_year"))
+                );
+                studentList.add(studentAdd);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error reading one student", e);
+        }
+        return studentList;
     }
 
     @Override
