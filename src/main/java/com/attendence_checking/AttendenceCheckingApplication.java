373dac4bea84fbb74c4f4f7c0908df90a22815ba
TsioryR98 <hei.tsiory.2@gmail.com>
2024-09-16 07:46:01 +0200

    feat:add project squellette

---
+++ src/main/java/com/attendence_checking/AttendenceCheckingApplication.java
@@ -0,0 +1,13 @@
+package com.attendence_checking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendenceCheckingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttendenceCheckingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/attendence_checking/AttendenceCheckingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attendence_checking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendenceCheckingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
