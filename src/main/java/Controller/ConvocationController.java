90f8e7bfeba23654a2a92fa9badeef95abc3557a
TsioryR98 <hei.tsiory.2@gmail.com>
2024-09-20 22:28:43 +0200

    feat:add controller for COR

--- src/main/java/Controller/ConvocationController.java
+++ src/main/java/Controller/ConvocationController.java
@@ -8,5 +8,29 @@ import java.util.List;
 
 @RestController
 public class ConvocationController {
+    private final ConvocationService convocationService;
 
+    public ConvocationController(ConvocationService convocationService) {
+        this.convocationService = convocationService;
+    }
+    @PostMapping("/convocations")
+    public void createConvocation(@RequestBody Convocation newConvocation){
+        convocationService.createConvocation(newConvocation);
+    }
+    @GetMapping("/convocations")
+    public List<Convocation> showAllConvocations(){
+        return  convocationService.showAllConvocations();
+    }
+    @PutMapping("/convocations/{convocationId}")
+    public Convocation updateConvocationById(@PathVariable int convocationId, @RequestBody Convocation convocationUpdate) {
+        return convocationService.updateConvocationById(convocationId,convocationUpdate);
+    }
+    @DeleteMapping("/convocations/{convocationId}")
+    public void deleteConvocationById(@PathVariable int convocationId) {
+        convocationService.deleteConvocationById(convocationId);
+    }
+    @GetMapping("/convocations/{convocationId}")
+    public Convocation readConvocationById(@PathVariable int convocationId) {
+        return convocationService.readConvocationById(convocationId);
+    }
 }
